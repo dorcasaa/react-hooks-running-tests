988338c6b7e564154f33bf78ec52c1ea3e8c1fa4
dorcas <dorcasaumaakamu@gmail.com>
2024-05-01 10:41:23 +0300

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Header(props) {
       return (
-        <h1>
+        <h1>hello from the Header!
           {/* uncomment the line below to pass the test */}
           {/* hello from the Header! */}
-        </h1>
+          </h1>
       )
 }
 
